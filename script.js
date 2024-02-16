8ec9d662124ea3c66b94ec6e6532eeca4f0748ad
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-17 01:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let inp = document.querySelector("#fname");
+		inp.addEventListener("blur",upper);
+		function upper(){
+			let ans = inp.value.toUpperCase();
+
+		inp.value = ans;
+		}
